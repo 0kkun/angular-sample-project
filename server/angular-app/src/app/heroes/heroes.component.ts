1a8cfa482835163a049db9287c398d985b177884
0kkun <shinji.okumoto.01@gmail.com>
2021-07-04 10:53:18 +0900

    add heroes interface and register module

--- server/angular-app/src/app/heroes/heroes.component.ts
+++ server/angular-app/src/app/heroes/heroes.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes',                 // コンポーネントのCSS要素セレクター。表示したい場所にこの名前で指定する。
+  templateUrl: './heroes.component.html', // コンポーネントのテンプレートファイルの場所
+  styleUrls: ['./heroes.component.css']   // コンポーネントのプライベートCSSスタイルの場所
 })
 export class HeroesComponent implements OnInit {
+
+  // HeroインターフェースでHero型に指定
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 

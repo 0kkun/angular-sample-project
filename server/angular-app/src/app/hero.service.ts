288fdf3c313b1e154ac8537cae5572287b152c5a
0kkun <shinji.okumoto.01@gmail.com>
2021-07-06 10:58:13 +0900

    DI message service to hero service

--- server/angular-app/src/app/hero.service.ts
+++ server/angular-app/src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { Observable, of } from 'rxjs'; // of関数を使って非同期処理でサーバーからのデータ取得
+import { MessageService } from './message.service';
 
 
 // 注入可能なサービスにすることを宣言
@@ -15,12 +16,14 @@ import { Observable, of } from 'rxjs'; // of関数を使って非同期処理で
  */
 export class HeroService {
 
-  constructor() { }
+  // シングルトンでサービスをDI
+  constructor(private messageService: MessageService) { }
 
   // モックヒーローの配列を出力する。Observableで非同期処理でデータを取得
   // Observableはヒーローの配列を出力するのを待っている状態
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 }

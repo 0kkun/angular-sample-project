72a233fd6d3f75d8a314ca7bb5586ed025f5052d
0kkun <shinji.okumoto.01@gmail.com>
2021-07-06 12:17:00 +0900

    edit dashboad component and html

--- server/angular-app/src/app/dashboard/dashboard.component.ts
+++ server/angular-app/src/app/dashboard/dashboard.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
+
+// ヒーロー名のリンクのグリッド表示
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  heroes: Hero[] = [];
 
-  ngOnInit(): void {
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
   }
 
+  getHeroes(): void {
+    // 配列を1番目と5番目でスライスし、トップヒーローの4つだけ（2番目、3番目、4番目、5番目）を返す
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes.slice(1, 5));
+  }
 }

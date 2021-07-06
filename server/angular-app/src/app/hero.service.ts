cb9489112f371fc3e0697910a3d63aef4eeeb1d7
0kkun <shinji.okumoto.01@gmail.com>
2021-07-06 10:29:06 +0900

    edit hero service

--- server/angular-app/src/app/hero.service.ts
+++ server/angular-app/src/app/hero.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
+
+// 注入可能なサービスにすることを宣言
 @Injectable({
   providedIn: 'root'
 })
+
+/**
+ * ヒーローデータのモックを返すサービスクラス
+ * コンポーネントからデータ取得ロジックを切り離す
+ */
 export class HeroService {
 
   constructor() { }
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
 }

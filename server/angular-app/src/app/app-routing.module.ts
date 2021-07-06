2179307106e3940a0260bc9127985534fc3fe5fb
0kkun <shinji.okumoto.01@gmail.com>
2021-07-06 12:04:12 +0900

    add route to routing module

--- server/angular-app/src/app/app-routing.module.ts
+++ server/angular-app/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HeroesComponent } from './heroes/heroes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'heroes', component: HeroesComponent }
+];
 
+// ルーターを初期化し、ブラウザのロケーションの変更を待機
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }

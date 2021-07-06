a6e6e47aac2a27e8e37eb619a91cfba8f02be8f2
0kkun <shinji.okumoto.01@gmail.com>
2021-07-06 11:03:56 +0900

    DI message service to message component

--- server/angular-app/src/app/messages/messages.component.ts
+++ server/angular-app/src/app/messages/messages.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
+
 export class MessagesComponent implements OnInit {
 
-  constructor() { }
+  // MessagesComponentを作成する際、シングルトンな MessageService インスタンスをそのプロパティへ注入
+  // messageService はhtmlテンプレート内でバインドして使用するため、publicの必要がある
+  // Angular はコンポーネント内の パブリック なプロパティのみをバインドする
+  constructor(public messageService: MessageService) { }
 
   ngOnInit(): void {
   }
